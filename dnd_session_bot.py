8a897e49515ce224e455a51b1b338475b9cf1c67
kell <imoen@disroot.org>
2021-01-05 20:55:38 +0300

    dnd_session_bot.py with help and start functions were added

---
+++ dnd_session_bot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    """Send a message when the command /start is issued"""
+    update.message.reply_text("Dark cycles, sunlander")
+
+def help(update, context):
+    """Send a help message when the command /help is issued"""
+    update.message.reply_text("This bot still do nothing")
+
+def main():
+    """Start bot"""
+    updater = Updater("", use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
